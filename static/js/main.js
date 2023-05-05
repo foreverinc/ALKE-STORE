8f18d795edde238f5dbef715c3adbc209f2db710
Forever Inc dev <foreverinc.dev@gmail.com>
2023-05-05 08:53:07 +0300

    save

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,47 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 10,
-        responsiveClass: true,
-        lazyLoad: true,
-        nav: true,
-        loop:true,
-        navText: ['<i class="fa-solid fa-angles-left  text-success"></i>', '<i class="fa-solid fa-angles-right  text-success"></i>'],
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 3,
-            },
-            1000: {
-                items: 5,
-            },
+      loop: true,
+      margin: 10,
+      responsiveClass: true,
+      lazyLoad: true,
+      nav: true,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 7000,
+      navText: [
+        '<i class="fa-solid fa-angles-left  text-success"></i>',
+        '<i class="fa-solid fa-angles-right  text-success"></i>',
+      ],
+      responsive: {
+        0: {
+          items: 1,
         },
+        600: {
+          items: 3,
+        },
+        1000: {
+          items: 5,
+        },
+      },
     });
 });
+
+
+function toggleTab() {
+  const tab = document.querySelector(".tab");
+  tab.classList.toggle("active");
+}
+
+
+$(document).ready(function () {
+  // Add hover behavior to dropdown menu
+  $(".dropdown").hover(
+    function () {
+      $(this).find(".dropdown-menu").stop(true, true).delay(0).fadeIn(300);
+    },
+    function () {
+      $(this).find(".dropdown-menu").stop(true, true).delay(0).fadeOut(300);
+    }
+  );
+});
+
