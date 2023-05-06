8349f3861bcbb0c0c6ec1fd07885046ae47fba18
Forever Inc dev <foreverinc.dev@gmail.com>
2023-05-06 11:42:24 +0300

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -93,12 +93,26 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'railway',
+            'USER': 'postgres',
+            'PASSWORD': 'SAAaFS640WNnTT9TU9mI',
+            'HOST': 'containers-us-west-185.railway.app',
+            'PORT': '6857',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
