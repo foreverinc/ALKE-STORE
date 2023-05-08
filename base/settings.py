b09a0419082816aebfca43f9b5856b67210cc6b9
Forever Inc dev <foreverinc.dev@gmail.com>
2023-05-08 08:06:40 +0300

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-if not DEBUG:
+if  DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
