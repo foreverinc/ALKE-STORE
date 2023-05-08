9fa972d8bcc9ffcf2eca17b6117716d2dd56ce51
Forever Inc dev <foreverinc.dev@gmail.com>
2023-05-08 08:35:33 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -55,7 +55,7 @@ class Product(models.Model):
         ('XXL','XXL'),
     )
     name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10000, decimal_places=2)
+    price = models.DecimalField(max_digits=1000, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     product_img = models.ImageField(upload_to='product_images/main/', null=True, verbose_name='Product picture', blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
